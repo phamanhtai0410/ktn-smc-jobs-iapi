5e81377bf6c0da69c651bdb86238fc32082e3e09
phongphong <uithuyphong>
2022-11-16 16:08:58 +0700

    [SMC Jobs]: change log print to debug

--- scripts/handle_logs.py
+++ scripts/handle_logs.py
@@ -35,7 +35,7 @@ from web3 import Web3
 from config import Config
 from logger import debug
 
-print(Config.REDIS_CLUSTER)
+debug(Config.REDIS_CLUSTER)
 if Config.SENTRY_DSN:
     sentry_sdk.init(Config.SENTRY_DSN)
 redis_cluster = RedisCluster(
@@ -110,9 +110,9 @@ class EventParser:
         
         for _field in _args:
             if isinstance(_args[_field], bytes):
-                print('before: ', _args[_field])
+                debug('before: ', _args[_field])
                 _args[_field] = _args[_field].decode("utf-8") 
-                print('after: ', _args[_field])
+                debug('after: ', _args[_field])
 
         if not args_fields or not dict_fields:
             return json.loads(Web3.toJSON({
@@ -205,16 +205,16 @@ class RedisState(EventScannerState):
                 self.state = json.loads(_state)
             else:
                 self.reset(self.init_block)
-            # print(f"Restored the state, previously {self.state['last_scanned_block']} blocks have been scanned")
+            # debug(f"Restored the state, previously {self.state['last_scanned_block']} blocks have been scanned")
         except (IOError, json.decoder.JSONDecodeError):
-            print(f"{self.address} - State starting from scratch")
+            debug(f"{self.address} - State starting from scratch")
             self.reset(self.init_block)
 
     def save(self):
         """Save everything we have scanned so far in a file."""
         _state = json.dumps(self.state)
         redis_cluster.set(self.key_state, _state)
-        # print(f'Ket: {self.key_state}')
+        # debug(f'Ket: {self.key_state}')
         self.last_save = time.time()
 
     #
@@ -270,13 +270,13 @@ class RedisState(EventScannerState):
             if _tx_hash:
                 _key = f'msp:msp_redlock\{self.address}:{_tx_hash}'
                 _lock = True #dlm.lock(_key, 300000)
-                print(f'{self.address} - 🔑 🔑 🔑 Key lock: {_key}')
+                debug(f'{self.address} - 🔑 🔑 🔑 Key lock: {_key}')
                 if _lock:
-                    print(
+                    debug(
                         f'{self.address} - \033[92m ✔✔✔ Process .................. {_tx_hash} \033[0m')
                     self.wk_handle.delay(json.dumps(_wk_event))
                 else:
-                    print(
+                    debug(
                         f'{self.address} - \033[93m ⚠⚠⚠ ______ Lock fail ______ {_tx_hash} \033[0m')
         except:
             sentry_sdk.capture_exception()
@@ -395,7 +395,7 @@ if __name__ == "__main__":
     while True:
         try:
             # Note that our chain reorg safety blocks cannot go negative 18435731
-            print(f'{contract} - get_last_scanned_block {state.get_last_scanned_block()}')
+            debug(f'{contract} - get_last_scanned_block {state.get_last_scanned_block()}')
 
             start_block = state.get_last_scanned_block()
 
@@ -408,24 +408,24 @@ if __name__ == "__main__":
                         "%d-%m-%Y")
                 else:
                     formatted_time = "no block time available"
-                print(
+                debug(
                     f"{contract} - Current block: {current} ({formatted_time}), blocks in a scan batch: {chunk_size}, events processed in a batch {events_count}")
 
 
-            print(f"{contract} - cron log start: {start_block} -> {end_block}")
+            debug(f"{contract} - cron log start: {start_block} -> {end_block}")
 
             result, total_chunks_scanned = provider.scanner.scan(
                 start_block,
                 end_block,
                 start_chunk_size=7,
                 progress_callback=_update_progress)
-            print(f'{contract} - done scan {result}')
+            debug(f'{contract} - done scan {result}')
         except:
             sentry_sdk.capture_exception()
             traceback.print_exc()
             old_rpc = f'{provider_rpc}'
             if not providers:
-                print(f"{contract} - Cannot switch rpc => retry current rpc")
+                debug(f"{contract} - Cannot switch rpc => retry current rpc")
                 sentry_sdk.capture_message(
                     "Cannot switch rpc => retry current rpc")
             else:
